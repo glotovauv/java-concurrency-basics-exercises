b1e6a0e0c2bd0c1c05d2be7c1e9f4a0a1af674e8
iugl0818 <yulya.glotova94@yandex.ru>
2020-01-29 22:30:18 +0400

    add Semaphore

--- src/main/java/com/netcracker/java_concurrency_basics/semaphore/Semaphore.java
+++ src/main/java/com/netcracker/java_concurrency_basics/semaphore/Semaphore.java
@@ -1,18 +1,28 @@
 package com.netcracker.java_concurrency_basics.semaphore;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class Semaphore {
     private final int permits;
+    private int count = 0;
 
     public Semaphore(int permits) {
         this.permits = permits;
     }
 
-    public void acquire() throws InterruptedException {
-        // Implement.
+    public synchronized void acquire() throws InterruptedException {
+        while (count == permits) {
+            wait();
+        }
+        count++;
+        System.out.println("acquire - " + count);
     }
 
-    public void release() {
-        // Implement.
+    public synchronized void release() {
+        count--;
+        System.out.println("release - " + count);
+        notifyAll();
     }
 
     public static void main(String[] args) {
@@ -20,16 +30,27 @@ class Semaphore {
         // The group of 3 threads should be able to execute the critical section.
         Semaphore semaphore = new Semaphore(3);
 
-        try {
-            semaphore.acquire();
+        Runnable runnable = () -> {
+            try {
+                semaphore.acquire();
 
-            System.out.println("Permitted.");
+                System.out.println(Thread.currentThread().getName() + " - Permitted.");
 
-            Thread.sleep(2000);
+                Thread.sleep(2000);
 
-            semaphore.release();
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
+                semaphore.release();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        };
+
+        List<Thread> threads = new ArrayList<>(10);
+        for (int i = 0; i < 10; i++) {
+            threads.add(new Thread(runnable, "thread-" + i));
+        }
+
+        for (Thread thread : threads) {
+            thread.start();
         }
     }
 }
