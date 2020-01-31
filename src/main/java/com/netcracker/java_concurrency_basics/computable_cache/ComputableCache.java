1fddd5ec2526b5613968b0c698b13822d6debd9f
iugl0818 <iuliya.glotova@netcracker.com>
2020-01-31 13:06:25 +0400

    computeIfAbsent

--- src/main/java/com/netcracker/java_concurrency_basics/computable_cache/ComputableCache.java
+++ src/main/java/com/netcracker/java_concurrency_basics/computable_cache/ComputableCache.java
@@ -1,25 +1,40 @@
 package com.netcracker.java_concurrency_basics.computable_cache;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class ComputableCache<K, V> {
-    private final Map<K, V> cache = new HashMap<>();
+    private final Map<K, V> cache = new ConcurrentHashMap<>();
+    private final Map<K, Object> locks = new ConcurrentHashMap<>();
 
-    public synchronized V compute(K key) {
-        V res = cache.get(key);
+    public V compute(K key) {
+        return cache.computeIfAbsent(key, this::computeExpensive);
+
+        /*V res = cache.get(key);
 
         if (res == null) {
-            res = computeExpensive(key);
-
-            cache.put(key, res);
-
-            System.out.println(Thread.currentThread().getName() + ": miss");
-        } else {
-            System.out.println(Thread.currentThread().getName() + ": hit");
+            synchronized (getLock(key)) {
+                res = cache.get(key);
+                if(res == null) {
+                    res = computeExpensive(key);
+                    cache.put(key, res);
+                    locks.remove(key);
+                } else {
+                    locks.remove(key);
+                }
+            }
         }
+        return res;*/
+    }
 
-        return res;
+    private Object getLock(K key) {
+        Object lock = locks.get(key);
+        if (lock == null) {
+            lock = new Object();
+            Object oldLock = locks.putIfAbsent(key, lock);
+            lock = oldLock == null ? lock : oldLock;
+        }
+        return lock;
     }
 
     private V computeExpensive(K key) {
